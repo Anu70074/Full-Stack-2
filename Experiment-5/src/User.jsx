f8dd177ea73fdfa32d9ed9de6bec21d97b276162
Anu <anubindal89305@gmail.com>
2026-02-23 15:47:52 +0530

    Update User.jsx

--- Experiment-5/src/User.jsx
+++ Experiment-5/src/User.jsx
@@ -1,15 +1,16 @@
 function User() {
   return (
     <div>
-      <h1>User name is Paras Manak</h1>
-      <h1>User name is Paras Manak</h1>
-      <h1>User name is Paras Manak</h1>
-      <h1>User name is Paras Manak</h1>
-      <h1>User name is Paras Manak</h1>
-      <h1>User name is Paras Manak</h1>
-      <h1>User name is Paras Manak</h1>
+      <h1>User name is ANU</h1>
+      <h1>User name is ANU</h1>
+      <h1>User name is ANU</h1>
+      <h1>User name is ANU</h1>
+      <h1>User name is ANU</h1>
+      <h1>User name is ANU</h1>
+      <h1>User name is ANU</h1>
     </div>
   );
 }
 
 export default User;
+
