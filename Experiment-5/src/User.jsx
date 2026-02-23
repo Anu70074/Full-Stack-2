d174ae2cdca2e2b707d4b6ad5f72262223e3aeb5
Anu <anubindal89305@gmail.com>
2026-02-23 15:46:33 +0530

    Add files via upload

---
+++ Experiment-5/src/User.jsx
@@ -0,0 +1,15 @@
+function User() {
+  return (
+    <div>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+    </div>
+  );
+}
+
+export default User;
